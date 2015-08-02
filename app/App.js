0a29eb8f653c1b021ba5d5057b92865ab54dea09
kadikraman <hellokadi@gmail.com>
2015-08-03 00:28:57 +0100

    Added Griddle for tables and an example table

--- app/App.js
+++ app/App.js
@@ -1,13 +1,36 @@
 /** @jsx React.DOM */
 var React = require('react');
+var Griddle = require('griddle-react');
 
+var data = [];
+for (var i = 1; i < 1000; i++) {
+	data.push({
+		id: i,
+		title: 'Title ' + i,
+		count: i * 1000
+	});
+}
+
+
+// Title of the page
+var Title = React.createClass({
+    render: function(){
+        return (<div><h1>Car Expenses Web</h1></div>)
+    }
+});
+
+// Data table for the expenses
+var Grid = React.createClass({
+   render: function(){
+       return (<Griddle results={data} resultsPerPage={20}/>)
+   }
+});
+
+// Container for the entire page
 var App = React.createClass({
 	render: function() {
-		return (
-			<h1>Hello world!</h1>
-		);
+		return  (<div><Title/><Grid/></div>);
 	}
-	
 });
 	
 module.exports = App;
--- readme.md
+++ readme.md
@@ -14,6 +14,11 @@ Run dev server:
 gulp
 ```
 
+## Debugging
+- gulp errors out on watch. This is because the system has a limit to how many files can be watched by a user. So we need to increase the max watcher siz
+```
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+```
 
 ## Credits:
 - boilerplate: https://github.com/christianalfoni/react-app-boilerplate
